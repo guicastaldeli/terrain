a1d6e346bb70e20369bd10485de1883ca0686899
gui castaldeli <guicastaldeli8@gmail.com>
2025-12-23 08:36:04 -0400

    font map working (at first try!)

--- root/src/main/com/app/root/_font/list.lua
+++ root/src/main/com/app/root/_font/list.lua
@@ -4,30 +4,10 @@ local fonts = {
         size = 24,
         path = "root/src/main/com/app/root/_font/fonts/arial.ttf"
     },
-    roboto = {
-        name = "Roboto",
+    comic_sans = {
+        name = "Comic Sans",
         size = 24,
-        path = "root/src/main/com/app/root/_font/fonts/roboto.ttf"
-    },
-    opensans = {
-        name = "Open Sans",
-        size = 22,
-        path = "root/src/main/com/app/root/_font/fonts/opensans.ttf"
-    },
-    consolas = {
-        name = "Consolas",
-        size = 20,
-        path = "root/src/main/com/app/root/_font/fonts/consolas.ttf"
-    },
-    montserrat_bold = {
-        name = "Montserrat Bold",
-        size = 32,
-        path = "root/src/main/com/app/root/_font/fonts/montserrat_bold.ttf"
-    },
-    fira_code = {
-        name = "Fira Code",
-        size = 18,
-        path = "root/src/main/com/app/root/_font/fonts/firacode.ttf"
+        path = "root/src/main/com/app/root/_font/fonts/comic.ttf"
     }
 }
 
