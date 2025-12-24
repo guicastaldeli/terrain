360e2da2b32b560a22dd8e5f5c14c3bbcc77f2fd
gui castaldeli <guicastaldeli8@gmail.com>
2025-12-24 07:43:57 -0400

    data added

---
+++ root/src/main/com/app/root/_data/axe_data.lua
@@ -0,0 +1,101 @@
+return {
+    {
+        indexTo = "axe0",
+        level = 0,
+        damage = 10,
+        speed = 1.0,
+        wood_multiplier = 1.0,
+        upgrade_cost = 100,
+        max_level = 10
+    },
+    {
+        indexTo = "axe1",
+        level = 1,
+        damage = 15,
+        speed = 1.2,
+        wood_multiplier = 1.2,
+        upgrade_cost = 200,
+        max_level = 10
+    },
+    {
+        indexTo = "axe2",
+        level = 2,
+        damage = 15,
+        speed = 1.2,
+        wood_multiplier = 1.2,
+        upgrade_cost = 200,
+        max_level = 10
+    },
+    {
+        indexTo = "axe3",
+        level = 3,
+        damage = 15,
+        speed = 1.2,
+        wood_multiplier = 1.2,
+        upgrade_cost = 200,
+        max_level = 10
+    },
+    {
+        indexTo = "axe4",
+        level = 4,
+        damage = 15,
+        speed = 1.2,
+        wood_multiplier = 1.2,
+        upgrade_cost = 200,
+        max_level = 10
+    },
+    {
+        indexTo = "axe5",
+        level = 5,
+        damage = 15,
+        speed = 1.2,
+        wood_multiplier = 1.2,
+        upgrade_cost = 200,
+        max_level = 10
+    },
+    {
+        indexTo = "axe6",
+        level = 6,
+        damage = 15,
+        speed = 1.2,
+        wood_multiplier = 1.2,
+        upgrade_cost = 200,
+        max_level = 10
+    },
+    {
+        indexTo = "axe7",
+        level = 7,
+        damage = 15,
+        speed = 1.2,
+        wood_multiplier = 1.2,
+        upgrade_cost = 200,
+        max_level = 10
+    },
+    {
+        indexTo = "axe8",
+        level = 8,
+        damage = 15,
+        speed = 1.2,
+        wood_multiplier = 1.2,
+        upgrade_cost = 200,
+        max_level = 10
+    },
+    {
+        indexTo = "axe9",
+        level = 9,
+        damage = 15,
+        speed = 1.2,
+        wood_multiplier = 1.2,
+        upgrade_cost = 200,
+        max_level = 10
+    },
+    {
+        indexTo = "axe10",
+        level = 10,
+        damage = 15,
+        speed = 1.2,
+        wood_multiplier = 1.2,
+        upgrade_cost = 200,
+        max_level = 10
+    },
+}
---
+++ root/src/main/com/app/root/_data/main_data.lua
@@ -0,0 +1,16 @@
+return {
+    player = {
+        wood = 0,
+        axe_level = 0,
+        current_axe = "axe0",
+        inventory = {
+            wood = 0
+        }
+    },
+    upgrades = {
+        costs = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 },
+        damage_increase = { 10, 15, 20, 25, 30, 35, 40, 45, 50, 55 },
+        speed_increase = { 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8 },
+        wood_multiplier = { 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8 }
+    }
+}
