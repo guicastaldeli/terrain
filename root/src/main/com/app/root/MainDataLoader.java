0a3a4df2109c496d72042429ee455714965a118d
gui castaldeli <guicastaldeli8@gmail.com>
2025-12-24 09:44:13 -0400

    update-14_01

---
+++ root/src/main/com/app/root/MainDataLoader.java
@@ -0,0 +1,48 @@
+package main.com.app.root;
+
+import org.luaj.vm2.Globals;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.jse.JsePlatform;
+
+public class MainDataLoader {
+    private static final String DATA_PATH = "root/src/main/com/app/root/_data/main_data.lua";
+
+    public static MainData load() {
+        try {
+            Globals globals = JsePlatform.standardGlobals();
+            LuaValue chunk = globals.loadfile(DATA_PATH);
+            chunk.call();
+
+            MainData data = new MainData();
+
+            /* Player */
+            LuaValue playerTable = globals.get("player");
+            data.setWood(playerTable.get("wood").toint());
+            data.setAxeLevel(playerTable.get("axe_level").toint());
+            data.setCurrentAxe(playerTable.get("current_axe").tojstring());
+
+            /* Upgrade */
+            LuaValue upgradesTable = globals.get("upgrades");
+            LuaValue costsTable = upgradesTable.get("costs");
+            int[] costs = new int[costsTable.length()];
+            for(int i = 1; i <= costsTable.length(); i++) {
+                costs[i-1] = costsTable.get(i).toint();
+            }
+            data.setUpgradeCosts(costs);
+
+            return data;
+        } catch(Exception e) {
+            System.err.println("Failed to load game data: " + e.getMessage());
+            return createDefaultGameData();
+        }
+    }
+
+    public static MainData createDefaultData() {
+        MainData data = new MainData();
+        data.setWood(0);
+        data.setAxeLevel(0);
+        data.setCurrentAxe("axe0");
+        data.setUpgradeCosts(new int[]{100, 200, 300, 400, 500, 600, 700, 800, 900, 1000});
+        return data;
+    }
+}
--- root/src/main/com/app/root/Upgrader.java
+++ root/src/main/com/app/root/Upgrader.java
@@ -1,5 +1,57 @@
 package main.com.app.root;
 
-public class Upgrader { //Main Upgrade Class
+import main.com.app.root.env.EnvCall;
+import main.com.app.root.env.EnvController;
+import main.com.app.root.env.EnvData;
+
+public class Upgrader {
+    MainData data;
+    private EnvController envController;
+
+    public Upgrader(EnvController envController) {
+        this.data = MainDataLoader.load();
+        this.envController = envController;
+    }
+
+    /**
+     * Upgrade
+     */
+    public boolean canUpgrade() {
+        Object axeInstance = envController.getEnv(EnvData.AXE).getInstance();
+        int currLevel = EnvCall.call(axeInstance, "getLevel");
+        int upgradeCost = EnvCall.call(axeInstance, "getUpgradeCost");
+
+        return data.getWood() >= upgradeCost;
+    }
+
+    public boolean upgradeAxe() {
+        if(!canUpgrade()) return false;
+
+        Object axeInstance = envController.getEnv(EnvData.AXE).getInstance();
+        int upgradeCost = EnvCall.call(axeInstance, "getUpgradeCost");
+        EnvCall.call(axeInstance, "upgrade");
+
+        saveData();
+        return true;
+    }
+
+    public void addWood(int amount) {
+        data.setWood(data.getWood() + amount);
+    }
     
+    public int getWood() {
+        return data.getWood();
+    }
+    
+    public int getAxeLevel() {
+        return axeController.getLevel();
+    }
+    
+    public int getNextUpgradeCost() {
+        return axeController.getUpgradeCost();
+    }
+
+    private void saveGameData() {
+        //todo this later... :P
+    }
 }
