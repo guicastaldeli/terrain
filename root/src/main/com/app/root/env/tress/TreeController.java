14b813672581403f0d82bca407b2eaa1352d57aa
gui castaldeli <guicastaldeli8@gmail.com>
2025-12-24 09:10:14 -0400

    update-13_09

--- root/src/main/com/app/root/env/tress/TreeController.java
+++ root/src/main/com/app/root/env/tress/TreeController.java
@@ -1,9 +1,20 @@
 package main.com.app.root.env.tress;
+import main.com.app.root.DependencyValue;
 import main.com.app.root.env.EnvInstance;
+import main.com.app.root.mesh.Mesh;
 
-public class TreeController implements EnvInstance<TreeController> { //Tree Controller
+public class TreeController implements EnvInstance<TreeController> {
+    @DependencyValue private Mesh mesh;
+
+    private TreeGenerator treeGenerator;
+
     @Override
     public TreeController getInstance() {
+        this.treeGenerator = new TreeGenerator();
         return this;
+    }
+
+    public TreeGenerator getGenerator() {
+        return treeGenerator;
     }
 }
--- root/src/main/com/app/root/env/tress/TreeGenerator.java
+++ root/src/main/com/app/root/env/tress/TreeGenerator.java
@@ -1,5 +1,128 @@
 package main.com.app.root.env.tress;
+import main.com.app.root.mesh.Mesh;
+import java.util.Random;
+import org.joml.Vector3f;
 
-public class TreeGenerator { //Tree Main Generator
+public class TreeGenerator {
+    private final TreeData treeData;
+    private final Vector3f position;
+    private final Mesh mesh;
+    private final String MESH_ID;
     
+    private String id;
+    private float currHealth;
+    private boolean isAlive;
+    private float respawnTimer;
+    private Random random;
+
+    public TreeGenerator(TreeData treeData, Vector3f position, Mesh mesh) {
+        this.treeData = treeData;
+        this.position = position;
+        this.mesh = mesh;
+        this.MESH_ID = "tree_" + System.currentTimeMillis() + "_" + treeData.getName();
+
+        this.currHealth = treeData.getHealth();
+        this.isAlive = true;
+        this.respawnTimer = 0;
+        this.random = new Random();
+
+        createMesh();
+    }
+
+    /**
+     * Mesh
+     */
+    private void createMesh() {
+        try {
+            mesh.addModel(MESH_ID, treeData.getModelPath());
+            System.out.println("Created mesh for " + treeData.getName() + " at [" + 
+                              position.x + ", " + position.z + "]");
+        } catch(Exception e) {
+            System.err.println("Failed to create mesh for " + treeData.getName() + ": " + e.getMessage());
+        }
+    }
+
+    private void destroyMesh() {
+        System.out.println("Mesh destroyed for " + treeData.getName());
+    }
+
+    public int takeDamage(int damage, int axeLevel) {
+        if(!isAlive) return 0;
+
+        if(axeLevel < treeData.getLevel()) {
+            System.out.println("Axe level " + axeLevel + " too low for tree level " + treeData.getLevel());
+            return 0;
+        }
+
+        currHealth -= damage;
+        System.out.println(treeData.getName() + " took " + damage + " damage. Health: " + currHealth + "/" + treeData.getHealth());
+        if(currHealth <= 0) {
+            isAlive = false;
+            respawnTimer = treeData.getRespawnTime();
+            destroyMesh();
+
+            int woodDrop = 
+                treeData.getWoodMin() +
+                random.nextInt(treeData.getWoodMax() - treeData.getWoodMax() + 1);
+
+            System.out.println(treeData.getName() + " destroyed! Dropping " + woodDrop + " wood.");
+            return woodDrop;
+        }
+
+        return 0;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public boolean isAlive() { 
+        return isAlive; 
+    }
+    
+    public int getLevel() { 
+        return treeData.getLevel(); 
+    }
+    
+    public Vector3f getPosition() { 
+        return new Vector3f(position); 
+    }
+    
+    public TreeData getData() { 
+        return treeData; 
+    }
+    
+    public String getMeshInstanceId() {
+        return MESH_ID;
+    }
+    
+    public float getHealthPercentage() {
+        return (currHealth / treeData.getHealth()) * 100.0f;
+    }
+
+    /**
+     * Respawn
+     */
+    private void respawn() {
+        isAlive = true;
+        currHealth = treeData.getHealth();
+        createMesh();
+        System.out.println(treeData.getName() + " has respawned at [" + position.x + ", " + position.z + "]");
+    }
+
+    /**
+     * Update
+     */
+    public void update(float deltaTime) {
+        if(!isAlive) {
+            respawnTimer -= deltaTime;
+            if(respawnTimer <= 0) {
+                respawn();
+            }
+        }
+    }
 }
